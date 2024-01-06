48c4426ee86efc8cdcdb0bf89ba05e8b23871171
Sarahgad <sarahgad91@gmail.com>
2024-01-07 00:50:56 +0200

    0

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,15 +9,16 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
-@app.teardown_appcontext
-def teardown():
-    """ remove the current storage"""
-    storage.close()
 
 @app.route("/api/v1/nop", strict_slashes=False)
 def custom_404():
     """handler for 404 errors"""
     return jsonify({"error": "Not found"})
+
+@app.teardown_appcontext
+def teardown(exception=None):
+    """ remove the current storage"""
+    storage.close()
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", threaded=True)

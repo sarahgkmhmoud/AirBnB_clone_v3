cfa211562aecf8d084aaf156ab8fe1c2a6f983bc
Manar Elsaid <elsaidmanar@gmail.com>
2024-01-06 20:14:41 +0000

    5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """main app"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -10,9 +10,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_appcontext():
-    """ remove the current SQLAlchemy Session"""
+def teardown():
+    """ remove the current storage"""
     storage.close()
+
+@app.route("/api/v1/nop", strict_slashes=False)
+def custom_404():
+    """handler for 404 errors"""
+    return jsonify({"error": "Not found"})
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", threaded=True)
